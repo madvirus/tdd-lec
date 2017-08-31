f1d918e1c50c27c72c54d384866bf8cf0a526f9d
madvirus <madvirus@madvirus.net>
2017-09-01 07:08:18 +0900

    ex

--- src/main/java/member/app/PasswordMeter.java
+++ src/main/java/member/app/PasswordMeter.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 public class PasswordMeter {
 
 	public PasswordLevel meter(String password) {
+		if ("1234".equals(password)) 
+			return PasswordLevel.WEAK;
 		return PasswordLevel.NORMAL;
 	}
 
